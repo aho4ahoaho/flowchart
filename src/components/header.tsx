5db4b2de24345bf949197c02ccecdfb7776e9659
aho4ahoaho <aho4ahoaho@outlook.jp>
2023-06-03 17:55:08 +0900

    ヘッダのデザイン変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,17 @@
 import style from "./header.module.scss";
 import Image from "next/image";
 import menuSvg from "../assets/menu.svg";
-
-type Props = {};
+import Link from "next/link";
 
 export const Header = () => {
 	return (
 		<header className={style.header}>
+			<Link href="/">
+				<h1>My</h1>
+			</Link>
 			<div className={style.menu}>
 				<Image src={menuSvg} alt="menu" />
 			</div>
-			<h1>My</h1>
-			<div className={style.rightmenu}></div>
 		</header>
 	);
 };
